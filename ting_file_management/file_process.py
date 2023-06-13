9ddc09575d9e6fc9be0eedaa71016cb95625b0c8
Josimar <josimarsaldanha@gmail.com>
2023-06-13 17:41:43 -0300

    feat: process

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,6 +1,23 @@
-def process(path_file, instance):
-    """Aqui irá sua implementação"""
+import sys
+from ting_file_management.file_management import txt_importer
 
+
+def process(path_file, instance):
+    if any(path_file in item for item in instance.queue):
+        return instance
+
+    content = txt_importer(path_file)
+    if content is not None:
+        new_dict = {
+            "nome_do_arquivo": path_file,
+            "qtd_linhas": len(content),
+            "linhas_do_arquivo": content
+        }
+        
+        instance.enqueue({path_file: new_dict})
+        sys.stdout.write(str(new_dict))
+    
+    return instance
 
 def remove(instance):
     """Aqui irá sua implementação"""
