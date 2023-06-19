e9dea5fd05184348eca3a0feadd36b1408443496
Josimar <josimarsaldanha@gmail.com>
2023-06-19 15:33:56 -0300

    feat: exist_word function

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,23 +1,21 @@
 def exists_word(word, instance):
-    results = []
-    
-    for file_data in instance.queue:
-        occurrences = []
-        
-        lines = file_data.values()
-        occurrences.append(lines)
-        # for line_number, line in enumerate(lines, start=1):
-        #     if word.lower() in line.lower():
-        #         occurrences.append({"linha": line_number})
-        
-        # if occurrences:
-        #     result = {
-        #         "palavra": word,
-        #         "arquivo": file_data["nome_do_arquivo"],
-        #         "ocorrencias": occurrences
-        #     }
-        #     results.append(result)
-    
+    results = [
+        {
+            "palavra": word,
+            "arquivo": file_key,
+            "ocorrencias": [
+                {"linha": line_number}
+                for line_number,
+                line in enumerate(file_data["linhas_do_arquivo"], start=1)
+                if word.lower() in line.lower()
+            ]
+        }
+        for file_dict in instance.queue
+        for file_key, file_data in file_dict.items()
+        if any(word.lower()
+               in line.lower() for line in file_data["linhas_do_arquivo"])
+    ]
+
     return results
 
 
